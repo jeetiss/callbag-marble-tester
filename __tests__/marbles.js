307f7fd13cef6ec7fbc5b7230d4048e5e3635102
jeetiss <za5rubley@gmail.com>
2018-10-21 15:49:20 +0300

    create marbles from events

--- __tests__/marbles.js
+++ __tests__/marbles.js
@@ -1,4 +1,4 @@
-import { parse } from '../src/marbles'
+import { parse, create } from '../src/marbles'
 
 const testCreators = {
   end: () => 'end',
@@ -15,7 +15,7 @@ const bindLastArg = (fn, arg) => (...args) => {
 
 const testParse = bindLastArg(parse, testCreators)
 
-test('async one after one', () => {
+test('parse async one after one', () => {
   const { frames, size } = testParse('aa|')
 
   expect(size).toBe(3)
@@ -24,14 +24,14 @@ test('async one after one', () => {
   expect(frames.get(2)).toEqual(['end'])
 })
 
-test('sync groop', () => {
+test('parse sync groop', () => {
   const { frames, size } = testParse('(aa|)')
 
   expect(size).toBe(1)
   expect(frames.get(0)).toEqual(['a', 'a', 'end'])
 })
 
-test('async groop', () => {
+test('parse async groop', () => {
   const { frames, size } = testParse('--(aa)--|')
 
   expect(size).toBe(6)
@@ -39,7 +39,7 @@ test('async groop', () => {
   expect(frames.get(5)).toEqual(['end'])
 })
 
-test('error', () => {
+test('parse error', () => {
   const { frames, size } = testParse('-a-a-x')
 
   expect(size).toBe(6)
@@ -48,16 +48,102 @@ test('error', () => {
   expect(frames.get(5)).toEqual(['error'])
 })
 
-test('empty', () => {
+test('parse empty', () => {
   const { frames, size } = testParse('|')
 
   expect(size).toBe(1)
   expect(frames.get(0)).toEqual(['end'])
 })
 
-test('error', () => {
+test('parse error', () => {
   const { frames, size } = testParse('x')
 
   expect(size).toBe(1)
   expect(frames.get(0)).toEqual(['error'])
+})
+
+test('create marble', () => {
+  const ast = {
+    frames: new Map([[0, [sink => sink(1, 'a')]], [1, [sink => sink(2)]]]),
+    size: 2,
+  }
+
+  const marbles = create(ast)
+
+  expect(marbles).toBe('a|')
+})
+
+test('create marble with async groop', () => {
+  const ast = {
+    frames: new Map([
+      [2, [sink => sink(1, 'a'), sink => sink(1, 'a'), sink => sink(1, 'a')]],
+      [5, [sink => sink(2)]],
+    ]),
+    size: 6,
+  }
+
+  const marbles = create(ast)
+
+  expect(marbles).toBe('--(aaa)--|')
+})
+
+test('create sync marble', () => {
+  const ast = {
+    frames: new Map([
+      [0, [sink => sink(1, 'a'), sink => sink(1, 'a'), sink => sink(2)]],
+    ]),
+    size: 1,
+  }
+
+  const marbles = create(ast)
+
+  expect(marbles).toBe('(aa|)')
+})
+
+test('create sync marble with error', () => {
+  const ast = {
+    frames: new Map([
+      [
+        0,
+        [sink => sink(1, 'a'), sink => sink(1, 'a'), sink => sink(2, 'error')],
+      ],
+    ]),
+    size: 1,
+  }
+
+  const marbles = create(ast)
+
+  expect(marbles).toBe('(aax)')
+})
+
+test('create async marble with error', () => {
+  const ast = {
+    frames: new Map([
+      [2, [sink => sink(1, 'a')]],
+      [4, [sink => sink(1, 'b')]],
+      [6, [sink => sink(1, 'c')]],
+      [8, [sink => sink(2, 'error')]],
+    ]),
+    size: 9,
+  }
+
+  const marbles = create(ast)
+
+  expect(marbles).toBe('--a-b-c-x')
+})
+
+test('create marble with custom values', () => {
+  const ast = {
+    frames: new Map([
+      [1, [sink => sink(1, 3)]],
+      [3, [sink => sink(1, 4)]],
+      [5, [sink => sink(1, 5)]],
+      [7, [sink => sink(2)]],
+    ]),
+    size: 8,
+  }
+
+  const marbles = create(ast)
+
+  expect(marbles).toBe('-a-b-c-|')
 })
--- src/marbles.js
+++ src/marbles.js
@@ -63,6 +63,40 @@ export const parse = (marbles, values = {}, error, userCreators) => {
   }
 }
 
-const create = ({ frames }) => {
+const eventNames = 'abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWYZ'
+
+export const create = ({ frames, size }) => {
   let marbles = ''
+  const events = new Map()
+  let next = 0
+
+  const getName = value => {
+    if (events.has(value)) {
+      return events.get(value)
+    } else {
+      const event = eventNames[next++]
+      events.set(value, event)
+      return event
+    }
+  }
+
+  const sink = (t, d) => {
+    if (t === 1) return getName(d)
+    if (t === 2 && !d) return '|'
+    if (t === 2 && d) return 'x'
+  }
+
+  for (let i = 0; i < size; ++i) {
+    if (frames.has(i)) {
+      const frame = frames
+        .get(i)
+        .map(creator => creator(sink))
+        .join('')
+      marbles += frame.length > 1 ? `(${frame})` : frame
+    } else {
+      marbles += '-'
+    }
+  }
+
+  return marbles
 }
